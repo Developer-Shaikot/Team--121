a80f900ed0f043c5820ee97d9578abcfc654bf18
Akash Kundu <saikatkundu43@gmail.com>
2021-06-04 20:19:36 +0600

    all

---
+++ Component/Devices/Device.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Device() {
+    return (
+        <View>
+            <Text>...</Text>
+        </View>
+    )
+}
